6e5e34c54d356778ab218b0212b23587d42cd2d3
Matthew Fitzpatrick <mrfitzpa@uvic.ca>
2025-03-13 18:38:48 +0000

    Attempt #5 at publishing package on PyPI.

--- run_tests.sh
+++ run_tests.sh
@@ -1,7 +1,6 @@
 cd tests
 COVERAGE_FILE=".coverage"
 python -m pytest --cov --cov-config=../.coveragerc -vv
-python -m coverage combine
 python -m coverage json
 python -m coverage report --fail-under=100
 unset COVERAGE_FILE
